d711f4b9dc367fdc0a31c50d054e6fe0431ec361
jeetee <github@jeetee.net>
2025-12-11 20:40:55 +0100

    2025.5.2

--- 2025/05-main.cpp
+++ 2025/05-main.cpp
@@ -42,6 +42,24 @@ static void logic(string fileName)
         valid_ranges.emplace_back(min, max);
     }
 
+    // Reduce ranges by merging overlaps (for part 2)
+    std::sort(std::execution::par, std::begin(valid_ranges), std::end(valid_ranges));
+    // As long as the start of the next interval is before my end, we can merge
+    // as it will always be equal to or above my own start due to the initial sorting
+    auto current_it = std::begin(valid_ranges);
+    auto next_it = valid_ranges.empty() ? std::end(valid_ranges) : std::next(current_it);
+    while (next_it != std::end(valid_ranges)) {
+        if (next_it->first <= current_it->second) {
+            current_it->second = std::max(current_it->second, next_it->second);
+        }
+        else { // Next does not overlap current
+            // Move it into the new (next) current position so the resulting vector will have no holes
+            *(++current_it) = std::move(*next_it);
+        }
+        ++next_it;
+    }
+    valid_ranges.erase(std::next(current_it), std::end(valid_ranges));
+
     int valid_count = 0;
     for (uint64_t num; input >> num;) {
         cout << '.' << std::flush; // Progress report
@@ -52,5 +70,12 @@ static void logic(string fileName)
         }
     }
 
-    cout << "\nNumber of valid IDs = " << valid_count << endl;
+    const uint64_t count_of_fresh_IDs = std::transform_reduce(std::execution::par, std::begin(valid_ranges), std::end(valid_ranges),
+                                                              0ul, std::plus{},
+                                                              [](std::pair<uint64_t, uint64_t> range){
+        return range.second - range.first + 1;
+    });
+
+    cout << "\nNumber of valid IDs = " << valid_count
+         << "\nNumber of fresh IDs = " << count_of_fresh_IDs << endl;
 }
