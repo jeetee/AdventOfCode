b3a3b277f2347f49c8ff8a9ea542dcef6bfc541e
jeetee <github@jeetee.net>
2025-12-17 23:55:32 +0100

    2025.9.2

--- 2025/09-main.cpp
+++ 2025/09-main.cpp
@@ -30,57 +30,65 @@ typedef struct point_s {
     int x, y;
 } point_t;
 
-/// \brief Check whether two line segments intersect
-/// \details See https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line_segment for formula
-/// \param p1 first point of Segment 1
-/// \param p2 second point of Segment 1
-/// \param p3 first point of Segment 2
-/// \param p4 second point of Segment 2
-bool segments_intersect(const point_t& p1, const point_t& p2, const point_t& p3, const point_t& p4)
-{
-    const int64_t denominator = (p1.x - p2.x) * (int64_t)(p3.y - p4.y)
-                              - ((p1.y - p2.y) * (int64_t)(p3.x - p4.x));
-    if (denominator == 0) {
-        // Parallel or coinciding lines, considered not crossing
-        return false;
-    }
-    const int64_t nominator_t = (p1.x - p3.x) * (int64_t)(p3.y - p4.y)
-                                - ((p1.y - p3.y) * (int64_t)(p3.x - p4.x));
-    const int64_t nominator_u = -(p1.x - p2.x) * (int64_t)(p1.y - p3.y)
-                                + ((p1.y - p2.y) * (int64_t)(p1.x - p3.x));
-
-    return (
-        ( // 0 <= t <= 1
-            ((denominator > 0) && (nominator_t >= 0) && (nominator_t <= denominator))
-         || ((denominator < 0) && (nominator_t <= 0) && (nominator_t >= denominator))
-        )
-        &&
-        ( // 0 <= u <= 1
-            ((denominator > 0) && (nominator_u >= 0) && (nominator_u <= denominator))
-         || ((denominator < 0) && (nominator_u <= 0) && (nominator_u >= denominator))
-        )
-    );
-}
-
 static void logic(string fileName)
 {
     cout << "Processing " << fileName << " file " << endl;
     std::ifstream input(fileName);
 
     vector<point_t> points;
+    int maxx = 0, maxy = 0;
     int x, y;
     while (input >> x) {
         input.ignore(); // Skip ','
         input >> y;
         points.emplace_back(x, y);
+        if (x > maxx) { maxx = x; }
+        if (y > maxy) { maxy = y; }
     }
+    cout << "Max X = " << maxx << " Max Y = " << maxy << endl;
 
-    vector<std::pair<point_t const*const, point_t const*const>> segments;
-    segments.reserve(points.size());
+    // For each y, register circumference points as ranges in x
+    vector<vector<std::pair<int, int>>> floorplan(maxy + 1);
+    auto register_circumference = [&floorplan](const point_t& p1, const point_t& p2){
+        // Process this circumference segment
+        if (p1.y == p2.y) {
+            // Same height, segment is entire x-range to register
+            floorplan[p1.y].emplace_back(std::min(p1.x, p2.x), std::max(p1.x, p2.x));
+        }
+        else { // p1->x == p2->x -> same column
+            for (int y = std::min(p1.y, p2.y) + 1; y < std::max(p1.y, p2.y); ++y) {
+                floorplan[y].emplace_back(p1.x, p1.x);
+            }
+        }
+    };
     for (auto p1 = std::cbegin(points), p2 = std::next(std::cbegin(points)); p2 != std::cend(points); ++p1, ++p2) {
-        segments.emplace_back(&*p1, &*p2);
+        register_circumference(*p1, *p2);
     }
-    segments.emplace_back(&*std::crbegin(points), &*std::cbegin(points)); // Add wrapping segment
+    // Same loop logic for last, closing, segment
+    register_circumference(*std::crbegin(points), *std::cbegin(points));
+
+    std::for_each(std::execution::par, std::begin(floorplan), std::end(floorplan), [](vector<std::pair<int, int>>& ranges){
+        std::sort(std::execution::par, std::begin(ranges), std::end(ranges));
+        bool merge = true;
+        auto current_it = std::begin(ranges);
+        auto next_it = ranges.empty() ? std::end(ranges) : std::next(current_it);
+        while (next_it != std::end(ranges)) {
+            if (merge) {
+                current_it->second = next_it->second;
+                if (next_it->first == next_it->second) {
+                    // Merged a range of 1 -> this was a vertical line; toggle filling
+                    merge = false;
+                }
+            }
+            else {
+                // Got a next range to start from -> make it consecutive
+                *(++current_it) = std::move(*next_it);
+                merge = true;
+            }
+            ++next_it;
+        }
+        ranges.erase(std::next(current_it), std::end(ranges));
+    });
 
     cout << "Finding largest rectangular area between two points " << endl;
     uint64_t max_area = 0, max_contained_area = 0;
@@ -90,27 +98,23 @@ static void logic(string fileName)
             if (area > max_area) {
                 max_area = area;
             }
+
             // Check whether this rectangle-combo is inside our polygon
-            // If we check whether it crosses any of the polygon boundaries we know it to be (partially) outside
-            // We need all corner points to be able to check
-            const point_t p12 { .x = p1->x, .y = p2->y }, p21 { .x = p2->x, .y = p1->y };
-            // Start with the wrapping segment
-            bool crossing = segments_intersect(*p1, p12, *std::crbegin(points), *std::cbegin(points))
-                | segments_intersect(p12, *p2, *std::crbegin(points), *std::cbegin(points))
-                | segments_intersect(*p2, p21, *std::crbegin(points), *std::cbegin(points))
-                | segments_intersect(p21, *p1, *std::crbegin(points), *std::cbegin(points));
-
-            if (!crossing) { // Check the other segments
-                crossing = std::any_of(std::execution::par, std::cbegin(segments), std::prev(std::cend(segments)), [&p1, &p2, &p12, &p21](auto segment){
-                    return segments_intersect(*p1, p12, *segment.first, *segment.second)
-                         | segments_intersect(p12, *p2, *segment.first, *segment.second)
-                         | segments_intersect(*p2, p21, *segment.first, *segment.second)
-                         | segments_intersect(p21, *p1, *segment.first, *segment.second);
+            const point_t top_left { .x = std::min(p1->x, p2->x), .y = std::min(p1->y, p2->y) };
+            const point_t bottom_right { .x = std::max(p1->x, p2->x), .y = std::max(p1->y, p2->y) };
+            const bool contained = std::all_of(
+                std::execution::par,
+                std::next(std::cbegin(floorplan), top_left.y),
+                std::next(std::cbegin(floorplan), bottom_right.y + 1), // +1 for inclusive check on height
+                [&top_left, &bottom_right](const vector<std::pair<int, int>>& row) {
+                    // Does width fit within a range of this row?
+                    const auto left = top_left.x;
+                    const auto right = bottom_right.x;
+                    return std::any_of(std::execution::par, std::cbegin(row), std::cend(row), [&left, &right](const std::pair<int, int>& range){
+                        return (left >= range.first) && (right <= range.second);
+                    });
                 });
-            }
-
-            if (!crossing) {
-                // We are considered inside, even if it's just a single shared edge overlap
+            if (contained) {
                 if (area > max_contained_area) {
                     max_contained_area = area;
                 }
